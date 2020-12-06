aca0d63030515af57711005dcd4f896b1b35af5b
mattveraldi <mattveraldi@gmail.com>
2020-12-06 19:12:09 +0100

    :fire: add hot reloading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-    return <h1>Welcome to Matteo Veraldi's React Toolchain.</h1>
+    return <h1>Welcome to Matteo Veraldi's React Sandbox.</h1>
 }
 
 export default App;
