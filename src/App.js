f91cc67d6cf329fb71cd7de7eaf38716789a97c8
mattveraldi <mattveraldi@gmail.com>
2020-12-14 09:37:16 +0100

    delete React folders

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./App.css";
-
-function App() {
-    return <h1>Welcome to Matteo Veraldi's Sandbox.</h1>
-}
-
-export default App;
--- src/__test__/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-test("it should be rendered", () => {
-  render(<App />);
-  expect(screen.getByText(/welcome to matteo veraldi's sandbox/i)).toBeInTheDocument();
-});
